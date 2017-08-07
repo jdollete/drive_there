3fe1d589d25a39440e705b3899e2baadc42fd3da
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-07 19:01:47 -0400

    Add vehicle id to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807221601) do
+ActiveRecord::Schema.define(version: 20170807225907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170807221601) do
     t.integer  "user_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "vehicle_id"
     t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
 
