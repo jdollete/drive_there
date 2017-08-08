c69500f5a3b470aefc938e8d4aa6de8564bf057d
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-07 20:36:02 -0400

    Limit array element to 5

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -7,7 +7,7 @@ class Vehicle < ApplicationRecord
   validates_uniqueness_of :serial_number
 
   def top_five_rentals
-    top_five = Reservation.group("vehicle_id").count.sort_by { |k, v| -v}
+    top_five = Reservation.group("vehicle_id").count.sort_by { |k, v| -v}[0..4]
     top_five.each do |element|
       Vehicle.where("id = ?", element[0])
     end
